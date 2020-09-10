9804fd480979f7afc8f34e6efd7970d01dab967d
dudwk814 <kj99658103@gmail.com>
2020-09-10 11:20:18 +0900

    2020-09-10 aop

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,5 @@ public class SpringConfig {
        // return new JpaMemberRepository(em);
 
     }*/
+
 }
